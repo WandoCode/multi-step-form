a5fa98afa7f7ecaca376e148f8b1f0f5de2bbc12
Wandole <mchirez@gmail.com>
2022-12-08 21:20:26 +0100

    Add the total price to the summary

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -1,14 +1,21 @@
 import {
   FormDatasType,
   FormProps,
+  periodTypes,
   pricesType,
   SummaryTypes,
 } from '../types/multiStepFormTypes'
+import { formatPrice, getTotalPrice } from '../utility/helpers'
 import CustomForm from './CustomForm'
 
 interface SummaryProps extends FormProps {
   allDatas?: FormDatasType
   prices: pricesType
+}
+
+const periodNoun = {
+  Monthly: 'month',
+  Yearly: 'year',
 }
 
 function Summary({
@@ -23,9 +30,12 @@ function Summary({
   const plan = allDatas?.selectPlan?.plan || 'Arcade' // TODO: ex de gardefous a retirer
   const period = allDatas?.selectPlan?.period || 'Monthly' //TODO: idem
   const addons = allDatas?.addOns?.choices || [] // TODO: idem
+  const planPrice = formatPrice(prices[period][plan])
+
+  const totalPrice = formatPrice(getTotalPrice(prices, addons, plan, period))
 
   const submitForm = () => {
-    // TODO: Valid form datas and show errors
+    // TODO: Valid form datas
     const datas: SummaryTypes = {
       confirm: true,
     }
@@ -37,14 +47,20 @@ function Summary({
     onGoBack()
   }
 
+  const handleGoPlan = () => {
+    onGoBack(2)
+  }
+
   const addonsDOM = addons.map((el) => {
+    const addonPrice = formatPrice(prices[period][el])
     return (
       <div className="summary__addonn-item" key={el}>
         <p className="summary__addons-text">{el}</p>
-        <p className="summary__addons-value">{prices[period][el]}</p>
+        <p className="summary__addons-value">+{addonPrice}</p>
       </div>
     )
   })
+
   return (
     <CustomForm
       title={title}
@@ -58,13 +74,17 @@ function Summary({
         <div className="summary__plan-wrapper">
           <div className="summary__plan-text">
             <p className="summary__plan">{`${plan}(${period})`}</p>
-            <a href="#" className="summary__plan-link">
+            <p className="summary__plan-link" onClick={handleGoPlan}>
               Change
-            </a>
+            </p>
           </div>
-          <p className="summary__plan-value">{prices[period][plan]}</p>
+          <p className="summary__plan-value">{planPrice}</p>
         </div>
         <div className="summary__addons">{addonsDOM}</div>
+      </div>
+      <div className="summary__total">
+        <p className="summary__total-text">Total (per {periodNoun[period]})</p>
+        <p className="summary__total-value">{totalPrice}</p>
       </div>
     </CustomForm>
   )
